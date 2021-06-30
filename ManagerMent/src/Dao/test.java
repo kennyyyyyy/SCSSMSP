70ca150f3e0671de9f44df46a6763ce5d19c68e6
Stranger123z <2801916353@qq.com>
2021-06-30 14:12:37 +0800

    jjj

--- ManagerMent/src/Dao/test.java
+++ ManagerMent/src/Dao/test.java
@@ -1,7 +1,9 @@
 package Dao;
 
 public class test {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("ssss");
+        System.out.println("12123");
     }
 }
